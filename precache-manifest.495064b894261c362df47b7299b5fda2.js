037582eb129fbebca5362bfc12e831a83b311340
kpkishankrishna <41905353+kpkishankrishna@users.noreply.github.com>
2022-12-13 21:11:06 +0530

    Updates

---
+++ precache-manifest.495064b894261c362df47b7299b5fda2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "043bc8098c0a314b9a4a",
+    "url": "/ai_knowledge_graph/static/js/main.a1dcf95d.chunk.js"
+  },
+  {
+    "revision": "a0addaa31777ae1312cf",
+    "url": "/ai_knowledge_graph/static/js/runtime~main.99c4dec9.js"
+  },
+  {
+    "revision": "8711127bf1a30a6312d3",
+    "url": "/ai_knowledge_graph/static/js/2.1194a71a.chunk.js"
+  },
+  {
+    "revision": "dcb96f88ced8892adc021576de4eef60",
+    "url": "/ai_knowledge_graph/index.html"
+  }
+];
